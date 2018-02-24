07f8427e5ac7a8fdc52e15f6eaa44cf29263e2fb
Paolo Ferretti <paolo.ferretti@fastmail.com>
2018-02-24 10:35:05 +0100

    Fixed indentation and readability, following Python community best practices

--- gameoflife.py
+++ gameoflife.py
@@ -5,14 +5,16 @@
 ROWS = 10
 COLUMNS = 10
 
+
 def empty_grid():
-   grid = []
-   for i in range(ROWS):
-      row=[]
-      for j in range(COLUMNS):
-          row.append(0)
-      grid.append(row)
-   return grid 
+    grid = []
+    for i in range(ROWS):
+        row = []
+        for j in range(COLUMNS):
+            row.append(0)
+        grid.append(row)
+    return grid
+
 
 class GameOfLife:
 
@@ -21,29 +23,29 @@ class GameOfLife:
         self.set_glider()
 
     def set_glider(self):
-        self.grid[3][1]=1
-        self.grid[3][2]=1
-        self.grid[3][3]=1
-        self.grid[2][3]=1
-        self.grid[1][2]=1
+        self.grid[3][1] = 1
+        self.grid[3][2] = 1
+        self.grid[3][3] = 1
+        self.grid[2][3] = 1
+        self.grid[1][2] = 1
 
-    def neighbours(self,i,j):
-        return self.grid[i-1][j-1] + \
-               self.grid[i][j-1] + \
-               self.grid[i+1][j-1] + \
-               self.grid[i-1][j] + \
-               self.grid[i+1][j] + \
-               self.grid[i-1][j+1] + \
-               self.grid[i][j+1] + \
-               self.grid[i+1][j+1]
+    def neighbours(self, i, j):
+        return self.grid[i - 1][j - 1] + \
+               self.grid[i][j - 1] + \
+               self.grid[i + 1][j - 1] + \
+               self.grid[i - 1][j] + \
+               self.grid[i + 1][j] + \
+               self.grid[i - 1][j + 1] + \
+               self.grid[i][j + 1] + \
+               self.grid[i + 1][j + 1]
 
     def next_generation(self):
         g = empty_grid()
-        for i in range(1,ROWS-1):
-            for j in range(1,COLUMNS-1):
-                nbs = self.neighbours(i,j)
+        for i in range(1, ROWS-1):
+            for j in range(1, COLUMNS-1):
+                nbs = self.neighbours(i, j)
                 cel = self.grid[i][j]
-                if cel  == 1:
+                if cel == 1:
                     if nbs == 2 or nbs == 3:
                         g[i][j] = 1
                     else:
@@ -63,11 +65,11 @@ class GameOfLife:
             s = s + '\n'
         return s
 
+
 gol = GameOfLife()
 print("Glider")
 print(gol)
-for c in range(1,5):
-   print("Generation",c)
-   gol.next_generation()
-   print(gol)
-
+for c in range(1, 5):
+    print("Generation", c)
+    gol.next_generation()
+    print(gol)
